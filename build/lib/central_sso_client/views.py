cc6ad9ad969f6ff00d79f9615c3482873f7d4145
OBED OTTO C <51106699+obedottoc@users.noreply.github.com>
2026-02-14 08:12:51 +0530

    Update views.py

--- build/lib/central_sso_client/views.py
+++ build/lib/central_sso_client/views.py
@@ -13,6 +13,7 @@ from .state import store_auth_flow, pop_and_validate_flow
 
 @require_http_methods(["GET"])
 def login(request: HttpRequest) -> HttpResponse:
+    return redirect("https://accounts.saveetha.in")
     try:
         sso = get_sso_settings()
         cfg = get_openid_config()
