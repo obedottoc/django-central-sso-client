0be99975cf5a91b2e2a65617d46cf2e0b376fb10
OBED OTTO C <51106699+obedottoc@users.noreply.github.com>
2026-02-14 13:39:55 +0530

    Implement session storage for auth flow

--- src/central_sso_client/state.py
+++ src/central_sso_client/state.py
@@ -8,14 +8,12 @@ SESSION_FLOW_KEY = "_sso_flow"
 
 
 def store_auth_flow(request: HttpRequest, state: str, nonce: str, code_verifier: str, next_url: str) -> None:
-    # request.session[SESSION_FLOW_KEY] = {
-    #     "state": state,
-    #     "nonce": nonce,
-    #     "code_verifier": code_verifier,
-    #     "next": next_url,
-    # }
-    return None
-
+    request.session[SESSION_FLOW_KEY] = {
+        "state": state,
+        "nonce": nonce,
+        "code_verifier": code_verifier,
+        "next": next_url,
+    }
 
 def pop_and_validate_flow(request: HttpRequest, state: str) -> Dict[str, str]:
     flow = request.session.pop(SESSION_FLOW_KEY, {})
